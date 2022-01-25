d9822665e65cd58bff47bf64702dfe351969ce05
AI-Michelinman <iphone74869@gmail.com>
2022-01-26 00:01:28 +0900

    app

---
+++ app.py
@@ -0,0 +1,77 @@
+import poloniex
+import time
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import numpy as np
+from binance.client import Client
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, LSTM,GRU,SimpleRNN
+from tensorflow.keras.optimizers import Nadam ,Adam
+from tensorflow.keras.callbacks import CSVLogger,EarlyStopping
+from tensorflow.keras import initializers
+import csv
+from sklearn.preprocessing import StandardScaler
+import math
+import requests
+import tweepy
+import json
+from requests_oauthlib import OAuth1Session
+import streamlit as st
+import yfinance as yf
+import streamlit.components.v1 as components
+from pivottablejs import pivot_ui
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import seaborn as sns
+from datetime import datetime, date
+
+sns.set_context("poster", 0.5)
+st.sidebar.markdown("## Settings")
+    
+st.title('自動車関連株式（工事中）')
+st.header('国内')
+        
+def show_heatmap(df):
+    fig, ax = plt.subplots(figsize=(10,10))
+    sns.heatmap(df.corr(), annot=True, ax=ax)
+    st.pyplot(fig)
+
+def main():
+    selected_len = st.sidebar.radio('期間',
+                             ['1ヶ月','1年','当会計年度','全てのデータ'])
+    if selected_len == '1ヶ月':
+        selected_len = '1mo'
+    elif selected_len == '1年':
+        selected_len = '1y'
+    elif selected_len == '当会計年度':
+        selected_len = 'ytd'
+    elif selected_len == '全てのデータ':
+        selected_len = 'max'
+        
+        
+    df_motor = pd.DataFrame(yf.download(['7203.T','7201.T','7261.T','7267.T','7270.T','7269.T','7211.T'], period=selected_len, interval = "1d")["Close"])
+    df_motor.columns = ['TOYOTA','日産','マツダ','ホンダ','ＳＵＢＡＲＵ','スズキ','三菱']
+
+    df_tire = pd.DataFrame(yf.download(['5108.T','5101.T','5110.T','5105.T'], period=selected_len, interval = "1d")["Close"])
+    df_tire.columns = ['ブリヂストン','横浜ゴム','住友ゴム','TOYO TIRE']
+    
+    st.subheader('自動車')
+    selected_targets_motor = st.multiselect('select targets', sorted(df_motor.columns))
+    view_motor = df_motor[selected_targets_motor]
+    st.line_chart(view_motor)
+
+    st.subheader('タイヤ')
+    selected_targets_tire = st.multiselect('select targets', sorted(df_tire.columns))
+    view_tire = df_tire[selected_targets_tire]
+    st.line_chart(view_tire)
+    
+    st.subheader('自動車とタイヤの相関関係')
+    df_corr = pd.concat([df_motor, df_tire], axis=1)
+    st.write(date.today())
+    show_heatmap(df_corr)
+    
+if __name__ == '__main__':
+    main()
+    st.sidebar.button('Reload')
