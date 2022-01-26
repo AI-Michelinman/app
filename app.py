3b5850230e5ac49a4eb7fc33127cc6ee4ddddd44
AI-Michelinman <96361372+AI-Michelinman@users.noreply.github.com>
2022-01-27 02:56:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import japanize_matplotlib
+import matplotlib as plt
 import streamlit as st
 import yfinance as yf
 import seaborn as sns
-
-sns.set(font='IPAexGothic')
 
 st.sidebar.markdown("## Settings")
