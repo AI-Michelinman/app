987fcea5207161cbd95fbd3b207ceb407c8f669d
AI-Michelinman <96361372+AI-Michelinman@users.noreply.github.com>
2022-01-28 00:55:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,67 +9,77 @@ st.title('Tatsuro note')
 
 
 def main():
-         
-    col1, col2, col3 = st.columns(3)
-    with col1:
-
-        agree = st.checkbox('投資関連')
+    st.header('お金メモ')
+    agree = st.checkbox('投資関連')
+    if agree == True :
+        st.text('天まで届く相場はない')
+        agree = st.checkbox('自動車関連')
         if agree == True :
-            st.write('マネー!')
+            st.write('海外メーカーも追加検討中')
+            st.sidebar.markdown("## Settings")   
 
-            agree = st.checkbox('自動車関連')
-            if agree == True :
-                st.write('海外メーカーも追加したいな')
-                st.sidebar.markdown("## Settings")   
-
-                col1_1, col1_2 = st.columns(3)
-                with col1_1:
-                    selected_len = st.radio('期間',
-                                            ['1ヶ月','1年','当会計年度','全てのデータ'])
-                    if selected_len == '1ヶ月':
-                        selected_len = '1mo'
-                    elif selected_len == '1年':
-                        selected_len = '1y'
-                    elif selected_len == '当会計年度':
-                        selected_len = 'ytd'
-                    elif selected_len == '全てのデータ':
-                        selected_len = 'max'
+            col1, col2 = st.columns(2)
+            with col1:
+                selected_len = st.radio('期間',
+                                        ['1ヶ月','1年','当会計年度','全てのデータ'])
+                if selected_len == '1ヶ月':
+                    selected_len = '1mo'
+                elif selected_len == '1年':
+                    selected_len = '1y'
+                elif selected_len == '当会計年度':
+                    selected_len = 'ytd'
+                elif selected_len == '全てのデータ':
+                    selected_len = 'max'
+            
+            with col2:
+                selected_interval = st.radio('時間足',
+                                        ['1日','5日','1週間','1ヶ月', '3ヶ月'])
+                if selected_interval == '1日':
+                    selected_interval = '1d'
+                elif selected_interval == '5日':
+                    selected_interval = '5d'
+                elif selected_interval == '1週間':
+                    selected_interval = '1wk'
+                elif selected_interval == '1ヶ月':
+                    selected_interval = '1mo'
+                elif selected_interval == '3ヶ月':
+                    selected_interval = '3mo'
+            
+            car_maker = ['TOYOTA','日産','マツダ','ホンダ','スバル','スズキ','三菱',
+                                'BS','横浜ゴム','住友ゴム','TOYO TIRE']
+            ticker = ['7203.T','7201.T','7261.T','7267.T','7270.T','7269.T','7211.T',
+                                                '5108.T','5101.T','5110.T','5105.T']
+            
+            df = pd.DataFrame(yf.download(ticker, period=selected_len, interval = selected_interval)["Close"])
+            df.columns = car_maker
+            
+            st.subheader('株価')
+            all = st.checkbox("Select all")
+            if all:
+                selected_options = st.multiselect('株価データ(データフレーム)', 
+                    sorted(df.columns),sorted(df.columns))
+            else:
+                selected_options =  st.multiselect('株価データ', 
+                    sorted(df.columns), default=sorted(df.columns))
                 
-                with col1_2:
-                    selected_interval = st.radio('時間足',
-                                            ['1日','5日','1週間','1ヶ月', '3ヶ月'])
-                    if selected_interval == '1日':
-                        selected_interval = '1d'
-                    elif selected_interval == '5日':
-                        selected_interval = '5d'
-                    elif selected_interval == '1週間':
-                        selected_interval = '1wk'
-                    elif selected_interval == '1ヶ月':
-                        selected_interval = '1mo'
-                    elif selected_interval == '3ヶ月':
-                        selected_interval = '3mo'
-                    
-                df = pd.DataFrame(yf.download(['7203.T','7201.T','7261.T','7267.T','7270.T','7269.T','7211.T',
-                                                    '5108.T','5101.T','5110.T','5105.T'], period=selected_len, interval = selected_interval)["Close"])
-                df.columns = ['TOYOTA','日産','マツダ','ホンダ','スバル','スズキ','三菱',
-                                    'BS','横浜ゴム','住友ゴム','TOYO TIRE']
-                st.subheader('株価')
-                all = st.checkbox("Select all")
-                if all:
-                    selected_options = st.multiselect('自動車メーカー', 
-                        sorted(df.columns),sorted(df.columns))
-                else:
-                    selected_options =  st.multiselect('自動車メーカー', 
-                        sorted(df.columns))
-                view_motor = df[selected_options]
-                st.line_chart(view_motor,width=1000)
-                stc.html('<div class="flourish-embed flourish-bar-chart-race" data-src="visualisation/8529391"><script src="https://public.flourish.studio/resources/embed.js"></script></div>',height=500)
+            st.dataframe(df[selected_options])
+            
+            st.text('株価データ(ラインチャート)')
+            view_motor = df[selected_options]
+            st.line_chart(view_motor,width=1000)
+
+            
+            st.text('ファイト〜！！')
+            stc.html('<div class="flourish-embed flourish-bar-chart-race" data-src="visualisation/8529391"><script src="https://public.flourish.studio/resources/embed.js"></script></div>',height=1000)
 
 
-    with col2:
-        st.header("データ分析")
-
-    with col3:
-        st.header("勉強")
+    st.header('データ分析')
+    agree = st.checkbox('機械学習')
+    if agree == True :
+        st.text('アイルビーバック！！')
+    st.header('お勉強中')
+    agree = st.checkbox('資格')
+    if agree == True :
+        st.text('いつやるの？')
 if __name__ == '__main__':
     main()
